4ff5a867a029a661eeb489348b5e6c3f463f4d8a
LORDNR <ratchanons.work@gmail.com>
2023-04-03 15:56:33 +0700

    🎉 Begin a project

---
+++ src/configs/environments.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config'
+
+const environments = {
+    PORT: process.env.PORT || 3000
+}
+
+export default environments
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('hello world');
+import Hapi from '@hapi/hapi'
+import { Request, ResponseToolkit } from 'hapi'
+import configs from './configs';
+
+const init = async () => {
+    const server: Hapi.Server = Hapi.server({
+        port: configs.environments.PORT,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request: Request, h: ResponseToolkit, err?: Error) => {
+            return { message: 'Hello world' };
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
