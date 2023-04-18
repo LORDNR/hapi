c607392090c0d4dd800795fe1e0f7bc3d93a0c1f
LORDNR <ratchanons.work@gmail.com>
2023-04-18 19:43:55 +0700

    🐛 add createdAt updatedAt

---
+++ src/database/entites/sharedProp.entity.ts
@@ -0,0 +1,17 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm'
+
+export class SharedProp {
+    @CreateDateColumn({
+        default: () => 'CURRENT_TIMESTAMP',
+        type: 'date',
+        name: 'created_at',
+    })
+    createdAt!: Date
+
+    @UpdateDateColumn({
+        default: () => 'CURRENT_TIMESTAMP',
+        type: 'date',
+        name: 'updated_at',
+    })
+    updatedAt!: Date
+}
