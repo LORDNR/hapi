857e64c7e6431d88ffa10a37f5e973d910fb4103
LORDNR <ratchanons.work@gmail.com>
2023-04-19 11:42:17 +0700

    🐛 query GET All, fix res GET{id}, add POST PATCH DELETE

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -12,18 +12,70 @@ const userController = () => {
             method: 'GET',
             path: '/users',
             handler: async (request: Request, h: ResponseToolkit, err?: Error) => {
-                const users = await userRepo.find();
-                return users;
+                const options = { where: { ...request.query } }
+
+                const users = await userRepo.find(options);
+                return h.response({ status: 'success', data: users }).code(200);
             }
         },
         {
             method: 'GET',
             path: '/users/{id}',
-            handler: async ({ params: { id } }: Request, h: ResponseToolkit, err?: Error) => {
-                const user = await userRepo.findOne({ where: { id } });
-                return user;
+            handler: async (request: Request, h: ResponseToolkit, err?: Error) => {
+                const id = request.params
+                const user = await userRepo.findOne({ where: id });
+                if (!user) {
+                    return h.response({ status: 'error', data: 'This user information does not exist.' }).code(400);
+                }
+                return h.response({ status: 'success', data: user }).code(200);
             }
         },
+        {
+            method: 'POST',
+            path: '/users',
+            handler: async (request: Request, h: ResponseToolkit, err?: Error) => {
+                const { firstName, lastName, email, birthOfDate } = request.payload as Partial<UserEntity>
+                const newUser = userRepo.create({ firstName, lastName, email, birthOfDate })
+                const savedUser = await userRepo.save(newUser)
+                return h.response({ status: 'success', data: savedUser }).code(201)
+            }
+        },
+        {
+            method: 'PATCH',
+            path: '/users/{id}',
+            handler: async (request: Request, h: ResponseToolkit, err?: Error) => {
+                const id = request.params
+                const userToUpdate = await userRepo.findOne({ where: id });
+
+                if (!userToUpdate) {
+                    return h.response({ status: 'error', data: 'This user information does not exist.' }).code(400);
+                }
+
+                const { firstName, lastName, birthOfDate, email } = request.payload as Partial<UserEntity>;
+                userToUpdate.firstName = firstName ?? userToUpdate.firstName;
+                userToUpdate.lastName = lastName ?? userToUpdate.lastName;
+                userToUpdate.birthOfDate = birthOfDate ?? userToUpdate.birthOfDate;
+                userToUpdate.email = email ?? userToUpdate.email;
+
+                const updatedUser = await userRepo.save(userToUpdate);
+                return h.response({ status: 'success', data: updatedUser }).code(200);
+            }
+        },
+        {
+            method: 'DELETE',
+            path: '/users/{id}',
+            handler: async (request: Request, h: ResponseToolkit) => {
+                const id = request.params;
+                const userToDelete = await userRepo.findOne({ where: id });
+
+                if (!userToDelete) {
+                    return h.response({ status: 'error', data: 'This user information does not exist.' }).code(400);
+                }
+
+                await userRepo.delete(id);
+                return h.response().code(204);
+            }
+        }
     ]
 }
 
